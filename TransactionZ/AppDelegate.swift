84209235e86d5dccfe3adecdb840518586f5a398
André Vellori <andre@vellori.it>
2019-02-07 23:36:35 +0000

    add code to refrain from loading the main storyboard when testing

--- TransactionZ/AppDelegate.swift
+++ TransactionZ/AppDelegate.swift
@@ -10,37 +10,50 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // The testing target has TESTING macro set, so we set a controller and system won't load main storyboard... meaning we get accurate unit testing coverage
+        #if TESTING
+        let controller = UIViewController()
+        controller.view.backgroundColor = UIColor.white
+        let label = UILabel()
+        label.text = "TESTING"
+        label.textColor = .red
+        controller.view.addSubview(label)
+        label.sizeToFit()
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
+        label.center = controller.view.center
+        #endif
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
