8c8a57c3c5d5ebc7b91d63c4564c69795c0a8e97
André Vellori <andre@vellori.it>
2019-02-08 00:19:16 +0000

    add image caching with NSCAche

--- TransactionZ/GeneralPurpose/ImageLoader.swift
+++ TransactionZ/GeneralPurpose/ImageLoader.swift
@@ -12,6 +12,7 @@ import ObjectiveC
 
 class ImageLoader {
     static let identifier = "imageLoad"
+    static let imageCache = NSCache<NSURL, UIImage>()
     var task: Cancellable?
     
     deinit {
@@ -19,15 +20,22 @@ class ImageLoader {
     }
     
     func get(url: URL, in imageView: UIImageView, networking: Networking = URLSession.shared) {
+        if let image = ImageLoader.imageCache.object(forKey: url as NSURL) {
+            imageView.image = image
+            print("Cache")
+            return
+        }
         let request = URLRequest(url: url)
         task = networking.perform(request: request, callback: { (response) in
             guard response.error == nil,
                 let data = response.data else {
                     return
             }
-            let image = UIImage(data: data)
-            DispatchQueue.main.async {
-                imageView.image = image
+            if let image = UIImage(data: data) {
+                ImageLoader.imageCache.setObject(image, forKey: url as NSURL)
+                DispatchQueue.main.async {
+                    imageView.image = image
+                }
             }
         })
         objc_setAssociatedObject(self, ImageLoader.identifier, self, .OBJC_ASSOCIATION_RETAIN)
